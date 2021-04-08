87b3c62a49e12621598f41597b2737c27487b314
Quino Guzmán <jguzman@bumon-web.com>
2021-04-08 10:38:43 +0200

    Add id user session

--- app/code/Apisearch/Searcher/Block/Integration.php
+++ app/code/Apisearch/Searcher/Block/Integration.php
@@ -4,10 +4,15 @@ namespace Apisearch\Searcher\Block;
 
 use Apisearch\Searcher\Helper\Data;
 use Magento\Framework\View\Element\Context;
+use Magento\Framework\Session\SessionManager;
 
 class Integration extends \Magento\Framework\View\Element\AbstractBlock
 {
     private $storeConfig;
+    /**
+     * @var SessionManager
+     */
+    private $_sessionManager;
 
     /**
      * @param Data $storeConfig
@@ -17,9 +22,11 @@ class Integration extends \Magento\Framework\View\Element\AbstractBlock
     public function __construct(
         Data $storeConfig,
         Context $context,
+        SessionManager $sessionManager,
         array $data = []
     ) {
         $this->storeConfig = $storeConfig;
+        $this->_sessionManager = $sessionManager;
         parent::__construct($context, $data);
     }
 
@@ -31,9 +38,10 @@ class Integration extends \Magento\Framework\View\Element\AbstractBlock
     public function toHtml()
     {
         $indexId = $this->storeConfig->getGeneralConfig('index_id');
+        $sessionID = $this->_session->getSessionId();
         $script =
             "<script type='application/javascript'>
-    
+    let user_session = '{$sessionID}';
     let as_snippet ='//pre.apisearch.cloud/{$indexId}.iframe.min.js?language=es';
     (function(d,t){var f=d.createElement(t),s=d.getElementsByTagName(t)[0];
     f.src=(('https:'===location.protocol)?'https:':'http:')+as_snippet;
